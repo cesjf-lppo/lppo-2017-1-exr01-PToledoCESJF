580a684a99d12e44d52583507103db72918c4f42
Paulo <Paulo@PToledo>
2017-02-15 07:42:46 -0200

    Carregando Driver Abrindo Conexão Criando a Tabela

--- src/lppoexercicio01/LppoExercicio01.java
+++ src/lppoexercicio01/LppoExercicio01.java
@@ -1,21 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lppoexercicio01;
 
-/**
- *
- * @author aluno
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class LppoExercicio01 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            System.out.println("Driver carregado com sucesso!");
+            
+            String url = "jdbc:derby://localhost:1527/lppoExercicio01";
+            Connection conexao = DriverManager.getConnection(url, "usuario", "senha");
+            System.out.println("Conexão aberta com sucesso!");
+            
+            String sql = "CREATE TABLE contatos (nome VARCHAR(30), sobrenome VARCHAR(30), Telefone VARCHAR(15), email VARCHAR(40))";
+            Statement operacao = conexao.createStatement();
+            operacao.executeUpdate(sql);
+            System.out.println("Tabela criada com sucesso!");
+            
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LppoExercicio01.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Driver não encontrado!");
+        } catch (SQLException ex) {
+            Logger.getLogger(LppoExercicio01.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("O banco não pode ser acessado!");
+        }
     }
     
 }
